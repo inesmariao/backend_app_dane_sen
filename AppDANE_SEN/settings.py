8999f52292914bcce6101adf7aff723c2cb4593e
inesmariao <63691309+inesmariao@users.noreply.github.com>
2025-08-01 15:11:44 -0500

    Prueba de configuración de CORS

--- AppDANE_SEN/settings.py
+++ AppDANE_SEN/settings.py
@@ -86,15 +86,16 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "https://app-diversa.vercel.app",
-    "http://localhost:3000",
-    "http://192.168.20.121:3000",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOW_ALL_ORIGINS = False
+# CORS_ALLOWED_ORIGINS = [
+#     "https://app-diversa.vercel.app",
+#     "http://localhost:3000",
+#     "http://192.168.20.121:3000",
+# ]
 
-CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_CREDENTIALS = False
 
 CORS_ALLOW_HEADERS = [
     'content-type',
